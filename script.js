81d172e50a5e7e714fb878d6be17484a21b32deb
fc115740Dharmasri <maneeshadharmasri@gmail.com>
2025-07-15 18:23:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+        const toggle = document.getElementById("nav-toggle");
+        const menu = document.getElementById("nav-menu");
+
+        toggle.addEventListener("click", () => {
+            menu.classList.toggle("active");
+        });
+    });
+
+
+
+
